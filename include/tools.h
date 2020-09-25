6d5ccb414c1caaf338d5bee5b42570804ec3f7c5
Kirill Konevets <kkonevets@gmail.com>
2020-09-25 22:52:37 +0300

    nice reinterpret_cast to vector

--- include/tools.h
+++ include/tools.h
@@ -4,7 +4,12 @@
 #define _GSC_INCLUDE_TOOLS_H
 
 #include <fstream>
+#include <iostream>
+#include <iterator>
 #include <optional>
+#include <sys/_types/_size_t.h>
+#include <utility>
+#include <vector>
 
 template <class T> struct EdgeItem {
   T first, second;
@@ -24,6 +29,36 @@ template <class T> struct EdgeItem {
     is.read(reinterpret_cast<char *>(&second), sizeof(T));
     if (is)
       return EdgeItem<T>{first, second};
+    else
+      return {};
+  }
+};
+
+template <class T> struct AdjItem {
+  T k;
+  std::vector<T> v;
+
+  explicit AdjItem(T k, std::vector<T> &&v) : k(k), v(v) {}
+  AdjItem() : k(0) {}
+
+  bool encode(std::ostream &os) {
+    T len{static_cast<T>(v.size())};
+    os.write(reinterpret_cast<char *>(&len), sizeof(T));
+    os.write(reinterpret_cast<char *>(&k), sizeof(T));
+    os.write(reinterpret_cast<char *>(v.data()), v.size() * sizeof(T));
+    return os.good();
+  }
+
+  static std::optional<AdjItem<T>> decode(std::istream &is) {
+    T len, k;
+    is.read(reinterpret_cast<char *>(&len), sizeof(T));
+    is.read(reinterpret_cast<char *>(&k), sizeof(T));
+
+    std::vector<T> v(len);
+    is.read(reinterpret_cast<char *>(v.data()), len * sizeof(T));
+
+    if (is)
+      return AdjItem<T>{k, std::move(v)};
     else
       return {};
   }
--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,24 @@
 #include "../include/tools.h"
 
 using edge_t = EdgeItem<std::uint32_t>;
+using adj_t = AdjItem<std::uint32_t>;
 
-int main() {
-  std::ifstream ifile("../data/edgelist.txt");
-  std::ofstream ofile("../data/edgelist.bin", std::ios::binary);
-  if (!ofile || !ifile) {
-    std::cerr << "could not open file" << std::endl;
-    return 1;
-  }
+void test_edge() {
+  {
+    std::ifstream ifile("../data/edgelist.txt");
+    std::ofstream ofile("../data/edgelist.bin", std::ios::binary);
+    if (!ofile || !ifile) {
+      std::cerr << "could not open file" << std::endl;
+      return;
+    }
 
-  for (edge_t edge; ifile >> edge;) {
-    if (!edge.encode(ofile)) {
-      std::cerr << "could not encode edge" << std::endl;
-      break;
+    for (edge_t edge; ifile >> edge;) {
+      if (!edge.encode(ofile)) {
+        std::cerr << "could not encode edge" << std::endl;
+        break;
+      }
     }
   }
-  ofile.close();
 
   {
     std::ifstream fin("../data/edgelist.bin", std::ios::binary);
@@ -34,6 +36,35 @@ int main() {
          edge = edge_t::decode(fin)) {
       auto &val = edge.value();
       std::cout << val.first << " " << val.second << std::endl;
+    }
+  }
+}
+
+int main() {
+  {
+    std::ofstream ofile("../data/edjlist.bin", std::ios::binary);
+    if (!ofile) {
+      std::cerr << "could not open file" << std::endl;
+      return 1;
+    }
+
+    ofile.unsetf(std::ios::skipws);
+    adj_t row{3, {1, 2, 3, 4, 5}};
+    if (!row.encode(ofile)) {
+      std::cerr << "could not encode row" << std::endl;
+    }
+  }
+
+  {
+    std::ifstream fin("../data/edjlist.bin", std::ios::binary);
+    for (auto row = adj_t::decode(fin); row.has_value();
+         row = adj_t::decode(fin)) {
+      auto &val = row.value();
+      std::cout << "k: " << val.k << std::endl;
+
+      std::copy(val.v.begin(), val.v.end(),
+                std::ostream_iterator<decltype(adj_t::k)>(std::cout, " "));
+      std::cout << std::endl;
     }
   }
 
